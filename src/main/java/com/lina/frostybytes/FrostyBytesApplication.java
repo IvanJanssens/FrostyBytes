7e0ade7be90846748d01cf7b8b3e1b48a5045383
Ivan <ivan@lemon.be>
2023-07-01 19:13:06 +0200

    merge upgrade axon
    gitignore
    
    gitignore
    
    gitignore
    
    
    gitignore
    
    
    gitignore
    
    
    gitignore

--- src/main/java/com/lina/frostybytes/FrostyBytesApplication.java
+++ src/main/java/com/lina/frostybytes/FrostyBytesApplication.java
@@ -2,8 +2,10 @@ package com.lina.frostybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories(basePackages = "com.lina.frostybytes.query")
 public class FrostyBytesApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lina/frostybytes/core_api/category/CommandModels.java
+++ src/main/java/com/lina/frostybytes/core_api/category/CommandModels.java
@@ -3,24 +3,28 @@ package com.lina.frostybytes.core_api.category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 import java.util.UUID;
 
 public record CommandModels() {
 
     public record addCategory(
-            @AggregateIdentifier UUID id,
+            @TargetAggregateIdentifier
+            @NotNull UUID id,
             @NotBlank String name,
             @NotBlank String icon
     ) {}
 
     public record updateCategory(
-            @AggregateIdentifier @NotNull UUID id,
+            @TargetAggregateIdentifier
+            @NotNull UUID id,
             @NotBlank String name,
             @NotBlank String icon
     ) {}
 
     public record deleteCategory(
-            @AggregateIdentifier @NotNull UUID id
+            @TargetAggregateIdentifier
+            @NotNull UUID id
     ) {}
 }
