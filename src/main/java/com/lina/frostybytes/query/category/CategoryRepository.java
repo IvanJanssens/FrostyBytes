9d979ad5f3bc794295f1e863dff57d4472507a88
Ivan <ivan@lemon.be>
2023-07-02 19:23:04 +0200

    remove old project

--- src/main/java/com/lina/frostybytes/query/category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lina.frostybytes.query.category;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.UUID;
-public interface CategoryRepository extends ReactiveCrudRepository<CategoryEntity, UUID> {
-}
